b237ac26521247bf23af52d39d7e688a902f7161
tanmancan <tanveer@bluetent.com>
2016-01-24 20:09:12 -0500

    Added request and async modules

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config');
+var request = require('request');
+var async = require('async');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
