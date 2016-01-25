adb0609b1806b895229913b000ec4882a0b62bed
tanmancan <tanveer@bluetent.com>
2016-01-24 20:06:33 -0500

    Added default config file

---
+++ default.config.js
@@ -0,0 +1,7 @@
+// Update api key and save as config.js
+module.exports = {
+	"apiKey" : "place-your-api-key-here",
+	"apiDomain" : "http://api.openweathermap.org/data/2.5/weather",
+	"apiLocation" : "?q=Baltimore",
+	"apiFormat" : "&units=imperial"
+};
