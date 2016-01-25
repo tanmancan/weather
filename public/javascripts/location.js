65a64e443a7f72b9134ab55a7692837fdcc8325e
tanmancan <tanveer@bluetent.com>
2016-01-24 23:42:02 -0500

    Some placeholder message

--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -1,31 +1,37 @@
 (function(){
-	var html = document.querySelector('html');
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(sendUserLocation, locationErr);
-	}
-	function locationErr(err) {
-		html.innerHTML = "Could not get user location!";
-	}
-	function sendUserLocation(pos) {
-		var currPos = pos;
-		console.log(currPos);
-		var xhr = new XMLHttpRequest();
+	window.onload = function() {
+		var html = document.querySelector('html');
+		if(navigator.geolocation) {
+			console.log(html);
+			document.body.firstChild.innerHTML = "Loading your current location weather...";
+			navigator.geolocation.getCurrentPosition(sendUserLocation, locationErr);
+		}
+		function locationErr(err) {
+			html.innerHTML = "ERROR:" + err;
+		}
+		function sendUserLocation(pos) {
+			var currPos = pos;
+			console.log(currPos);
+			var xhr = new XMLHttpRequest();
 
-		xhr.onreadystatechange = function() {
-			console.log(xhr);
-			switch (xhr.status) {
-				case 200:
-					console.log(xhr.status);
-					console.log(xhr.responseText);
-					html.innerHTML = xhr.responseText;
-					break;
-				default:
-					html.innerHTML = "Could not load weather info!";
-					break;
-			}
-		};
-		xhr.open('POST', '/', true);
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send("lat=" + encodeURIComponent(currPos.coords.latitude) + "&lon=" + encodeURIComponent(currPos.coords.longitude));
+			xhr.onreadystatechange = function() {
+				console.log(xhr);
+				switch (xhr.status) {
+					case 200:
+						console.log(xhr.status);
+						console.log(xhr.responseText);
+						html.innerHTML = xhr.responseText;
+						break;
+					case 400:
+					case 403:
+					case 500:
+						html.innerHTML = "Could not load weather info!";
+						break;
+				}
+			};
+			xhr.open('POST', '/', true);
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhr.send("lat=" + encodeURIComponent(currPos.coords.latitude) + "&lon=" + encodeURIComponent(currPos.coords.longitude));
+		}
 	}
 })();
