781a32f56ac11b05a09f5fc0426cb8ee7bc283cc
tanmancan <tanveer@bluetent.com>
2016-01-24 20:10:59 -0500

    Added ajax req for openweather api, moved render to callback

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,27 @@ var async = require('async');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	async.parallel([
+		function(callback) {
+			var path = config.apiDomain + config.apiLocation + config.apiFormat + '&APPID=' + config.apiKey;
+			request(path, function(err, res, body) {
+				if(err) {console.log(err); callback(true); return;}
+				obj = JSON.parse(body);
+				callback(false, obj);
+				console.log(obj);
+			});
+		}
+	],
+
+	function(err, results) {
+		var weather;
+		if(err) {console.log(err); res.send(500, 'Server Error'); return;}
+		weather = results[0];
+		console.log(weather);
+  		res.render('index', weather);
+	}
+	);
+
 });
 
 module.exports = router;
