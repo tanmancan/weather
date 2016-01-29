33d99588a5b43cc11fb7cf63159a5dacda6c9816
tanmancan <tanveer@bluetent.com>
2016-01-29 10:37:40 -0500

    Added forecast

--- default.config.js
+++ default.config.js
@@ -1,7 +1,8 @@
 // Update api key and save as config.js
 module.exports = {
 	"apiKey" : "place-your-api-key-here",
-	"apiDomain" : "http://api.openweathermap.org/data/2.5/weather",
+	"apiWeather" : "http://api.openweathermap.org/data/2.5/weather",
+	"apiForecast" : "http://api.openweathermap.org/data/2.5/forecast",
 	"apiLocation" : "?q=Baltimore",
-	"apiFormat" : "&units=imperial"
+	"apiUnits" : "&units=imperial"
 };
--- routes/weather.js
+++ routes/weather.js
@@ -12,13 +12,21 @@ module.exports = {
 
 		async.parallel([
 			function(callback) {
-				var path = config.apiDomain + apiLocation + config.apiFormat + '&APPID=' + config.apiKey;
+				var path = config.apiWeather + apiLocation + config.apiUnits + '&APPID=' + config.apiKey;
 				request(path, function(err, res, body) {
 					if(err) {console.log(err); callback(true); return;}
 					obj = JSON.parse(body);
 					callback(false, obj);
 				});
-			}
+			},
+			function(callback) {
+				var path = config.apiForecast + apiLocation + config.apiUnits + "&APPID=" + config.apiKey;
+				request(path, function(err, res, body) {
+					if(err) {console.log(err); callback(true); return;}
+					obj = JSON.parse(body);
+					callback(false, obj);
+				});
+			},
 		],
 
 		function(err, results) {
@@ -27,6 +35,7 @@ module.exports = {
 
 			if(err) {console.log(err); res.send(500, 'Server Error'); return;}
 			weather = results[0];
+			weather.forecast = results[1];
 
 			windDirection = weather.wind.deg;
 
@@ -40,6 +49,29 @@ module.exports = {
 			weather.wind.direction = compassDirection(windDirection);
 
 			weather.main.temp = Math.round(weather.main.temp);
+
+			var dailyForecast = [];
+			var today = new Date();
+
+			Object.keys(weather.forecast.list).forEach(function(key) {
+				var _this = weather.forecast.list[key];
+				var date = new Date(_this.dt_txt),
+					day = date.getDay;
+
+
+				windDirection = _this.wind.deg;
+				_this.wind.direction = compassDirection(windDirection);
+
+				_this.main.temp = Math.round(_this.main.temp);
+
+				_this.date = date.toDateString();
+
+				if (date.getHours() === 15 && today.getDate() != date.getDate()) {
+					dailyForecast.push(_this);
+				}
+			});
+
+			weather.forecast_daily = dailyForecast;
 
 	  		res.send(weather);
 		}
